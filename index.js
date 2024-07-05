587bab84c157de5b9c66b58dbf802612b5d587c2
Diego Munoz <diego51alejo@gmail.com>
2024-07-05 08:34:59 -0500

    upload project

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const routerApi = require('./routes');
+const cors = require('cors')
+
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'http://myapp.co']
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin) || !origin){
+      callback(null,true)
+    } else {
+      callback(new Error ('not allowed'))
+    }
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+routerApi(app);
+
+app.use(cors(options))
+
+app.use(logErrors)
+app.use(boomErrorHandler)
+app.use(errorHandler)
+
+app.listen(port, () => {
+  console.log('http://localhost:' +  port);
+});
